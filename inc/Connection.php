7d8b71d831e5a0acf2e857d956cb1e91e66fda14
Abdul Majeed Shehzad <shehzad3880@gmail.com>
2017-08-21 01:08:01 +0500

    Added files

---
+++ inc/Connection.php
@@ -0,0 +1,14 @@
+<?php
+$dbName 	= "cslib"	 ;
+$dbServer   = "localhost";
+$dbUserName = "root"	 ;
+$dbPassword = ""		 ;
+
+$db = new mysqli($dbServer, $dbUserName, $dbPassword, $dbName);
+
+if ($db->connect_errno)
+{
+	exit("Data Base Connection Failed. Reason: " .$db->connect_error);
+}
+
+?>
